959fde878eaec6c4b77315d790efca8d93ce53a7
Ayushi Solani <solaniayushi.04@gmail.com>
2026-01-30 18:50:47 +0530

    day 1: task logic and UI

--- popup.js
+++ popup.js
@@ -1,9 +1,41 @@
-document.getElementById("btn").addEventListener("click", () => {
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    const tab = tabs[0];
-    document.getElementById("title").innerText =
-      "Title: " + tab.title;
-    document.getElementById("url").innerText =
-      "URL: " + tab.url;
-  });
+const input = document.getElementById("task");
+const display = document.getElementById("currentTask");
+const btn = document.getElementById("saveBtn");
+const tabInfo = document.getElementById("tabInfo");
+const focusStatus = document.getElementById("focusStatus");
+
+
+btn.addEventListener("click", () => {
+  const value = input.value.trim();
+  if (!value) return;
+
+  localStorage.setItem("currentTask", value);
+  display.innerText = "Current task: " + value;
 });
+
+const savedTask = localStorage.getItem("currentTask");
+if (savedTask) {
+  input.value = savedTask;
+  display.innerText = "Current task: " + savedTask;
+}
+
+chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+  const tab = tabs[0];
+  const title = tab.title.toLowerCase();
+  const url = tab.url.toLowerCase();
+
+  tabInfo.innerText = "Current tab: " + tab.title;
+
+  const task = localStorage.getItem("currentTask");
+  if (!task) return;
+
+  if (title.includes(task.toLowerCase()) || url.includes(task.toLowerCase())) {
+    focusStatus.innerText = " Looks relevant to your task";
+    focusStatus.style.color = "green";
+  } else {
+    focusStatus.innerText = " This may distract you";
+    focusStatus.style.color = "red";
+  }
+});
+
+
